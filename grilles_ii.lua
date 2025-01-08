f06a7fffb0a90f2078dc5ece5cc23fc2d943f96e
Ema <97935484+OneTrickPoro@users.noreply.github.com>
2025-01-08 20:39:56 +0100

    Grilles i, ii and grilles minx i
    they need a lot of refinements

---
+++ grilles_ii.lua
@@ -0,0 +1,52 @@
+puzzles:add{
+  id = 'grilles_ii',
+  name = 'Grilles Cube II',
+  version = '1.0.0',
+  ndim = 3,
+  colors = 'cube',
+  build = function (self)
+    local sym = cd'bc3'
+    local shape = lib.symmetries.cubic.cube()
+    self:carve(shape:iter_poles())
+
+    self:carve(sym:orbit(sym.oox.unit))
+
+    self.axes:add(sym:orbit(sym.oox.unit),{1,0.763,-0.763,-1})
+
+    for _, ax, tw in sym:orbit(self.axes[1], rot{from ='x', to = 'y'}) do
+      self.twists:add(ax,tw, { gizmo_pole_distance = 1 })
+    end
+
+    self.axes:add(sym:orbit(sym.xoo.unit),{ 1.0232,1.454,1.732})
+
+    for _, ax, tw in sym.chiral:orbit(self.axes[sym.xoo.unit], sym:thru(3, 2)) do
+      self.twists:add(ax, tw, { gizmo_pole_distance = 1.2 })
+    end
+  end,
+
+  tags = {
+    builtin = '2.0.0',
+    external = { pCubes = 'cercare', '!hof', '!mc4d', museum =6789, '!wca' },
+
+    author = { "Emanuele Battistin" },
+    inventor = "Mr. Fork",
+
+    'type/puzzle',
+    'shape/3d/platonic/cube',
+    algebraic = {
+      'doctrinaire', 'pseudo/doctrinaire',
+      '!abelian', '!fused', '!orientations/non_abelian', '!trivial', '!weird_orbits',
+    },
+    axes = { '3d/elementary/cube', '!hybrid', '!multicore' },
+    colors = { '!multi_per_facet', '!multi_facet_per' },
+    completeness = { '!super', '!real', '!laminated', '!complex' },
+    cuts = { 'depth/deep/to_adjacent', '!stored', '!wedge' },
+    turns_by = { 'face', 'vertex' },
+    'experimental',
+    '!canonical',
+    '!family',
+    '!variant',
+    '!meme',
+    '!shapeshifting',
+  },
+}
