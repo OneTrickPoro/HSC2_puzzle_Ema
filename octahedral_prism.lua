b47446289d5a06bb74a1844b1ee01db8549808cd
Ema <97935484+OneTrickPoro@users.noreply.github.com>
2025-01-10 00:24:18 +0100

    iiiiit's workinggggggg
    Still need refinement

---
+++ octahedral_prism.lua
@@ -0,0 +1,26 @@
+puzzles:add({
+    id = 'octahedral_prism',
+    version = '0.1.0',
+    name = 'Octahedral prism',
+    ndim = 4,
+    tags = { 'experimental' },
+    build = function(p)
+      local sym = cd{4,3,2}
+      local pr = sym.ooox.unit
+      local fo = sym.xooo.unit
+      p:carve(sym:orbit(pr))
+      p:carve(sym:orbit(fo))
+
+      p.axes:add(sym:orbit(pr), {1, 0.5, 0, -0.5, -1})
+      p.axes:add(sym:orbit(fo), {1, 0.33, -0.33, -1})
+      
+      for _, axis, twist_transform in sym.chiral:orbit(p.axes[pr], sym:thru(2, 1)) do
+        p.twists:add(axis, twist_transform, {gizmo_pole_distance = 1})
+      end
+
+      for _, axis, twist_transform in sym.chiral:orbit(p.axes[fo], sym:thru(3, 2)) do
+        p.twists:add(axis, twist_transform, {gizmo_pole_distance = 0.1})
+      end
+    end,
+  })
+  
