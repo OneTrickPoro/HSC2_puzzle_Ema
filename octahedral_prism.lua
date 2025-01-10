e4efa2bf25969a9df3317fde736750b89b4f9e3a
Ema <97935484+OneTrickPoro@users.noreply.github.com>
2025-01-10 01:07:38 +0100

    Update octahedral_prism.lua
    fixed!

--- octahedral_prism.lua
+++ octahedral_prism.lua
@@ -14,13 +14,21 @@ puzzles:add({
       p.axes:add(sym:orbit(pr), {1, 0.5, 0, -0.5, -1})
       p.axes:add(sym:orbit(fo), {1, 0.33, -0.33, -1})
       
+      -- rotate around octa vertex
       for _, axis, twist_transform in sym.chiral:orbit(p.axes[pr], sym:thru(2, 1)) do
         p.twists:add(axis, twist_transform, {gizmo_pole_distance = 1})
       end
-
+      -- rotate octa prism around ax of prism
+      for _, axis, twist_transform in sym.chiral:orbit(p.axes[pr], sym:thru(3,2)) do
+        p.twists:add(axis, twist_transform, {gizmo_pole_distance = 1})
+      end
+      -- rotate around octa vertex
       for _, axis, twist_transform in sym.chiral:orbit(p.axes[fo], sym:thru(3, 2)) do
-        p.twists:add(axis, twist_transform, {gizmo_pole_distance = 0.1})
+        p.twists:add(axis, twist_transform, {gizmo_pole_distance = 0.5})
+      end
+      -- flip prism
+      for _, axis, twist_transform in sym.chiral:orbit(p.axes[fo], sym:thru(4,3)) do
+        p.twists:add(axis, twist_transform, {gizmo_pole_distance = 0.5})
       end
     end,
   })
-  
